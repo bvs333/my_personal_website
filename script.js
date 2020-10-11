0d4ae04272a1e120e49dbf3fc3f30138eb1ca1c3
Glitch (hello-webpage) <none>
2020-10-11 06:21:43 +0000

    🐪👦 Checkpoint ./index.html:23592034/508 ./script.js:23592034/486

--- script.js
+++ script.js
@@ -3,3 +3,17 @@
 
 // prints "hi" in the browser's dev tools console
 console.log("hi");
+
+
+var i = 0;
+//var txt = 'Lorem ipsum typing effect!'; /* The text */
+var txt = ' I\'m a Nature lover , explorer and a coding enthusiast. I also like travelling to new places .In my free time I watch TV series .'
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("moving").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
